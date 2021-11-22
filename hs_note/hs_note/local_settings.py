41f3c329934aabd236ce69d60061f6cfdc07a0e9
hs <641085132@qq.com>
2021-11-22 10:48:14 +0800

    local

---
+++ hs_note/hs_note/local_settings.py
@@ -0,0 +1,7 @@
+# mysql settings
+MYSQL_ENGINE = 'django.db.backends.mysql'
+MYSQL_NAME = 'HelloDjango'
+MYSQL_HOST = '192.168.16.158'
+MYSQL_PORT = '3306'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'anzer!123'
